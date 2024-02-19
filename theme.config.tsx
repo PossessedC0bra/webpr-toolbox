463dafc9146296c187096d0861f0056194ffced8
Yannick Koller <47781849+PossessedC0bra@users.noreply.github.com>
2024-02-19 16:52:33 +0100

    feat: add introduction and week 1

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,17 +2,11 @@ import React from 'react'
 import { DocsThemeConfig } from 'nextra-theme-docs'
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span><b>WEBPR</b></span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: 'https://github.com/possessedc0bra/webpr-quiz',
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
-  footer: {
-    text: 'Nextra Docs Template',
-  },
+  docsRepositoryBase: 'https://github.com/possessedc0bra/webpr-quiz/tree/main',
 }
 
 export default config
