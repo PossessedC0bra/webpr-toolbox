a9c8e9097da804e9cb398a44b879c3604cea56b1
Yannick Koller <47781849+PossessedC0bra@users.noreply.github.com>
2024-02-25 18:39:35 +0100

    feat: make questions interactive (#2)

---
+++ components/Question.jsx
@@ -0,0 +1,63 @@
+import {useState} from "react";
+import {Button} from "./ui/button";
+import {Code} from "nextra/components";
+import {CheckSquare, Play, X} from "lucide-react";
+import {Tooltip, TooltipContent, TooltipProvider, TooltipTrigger} from "./ui/tooltip";
+
+const ResultButton = ({expectedValue, actualValue, children}) => {
+    return <TooltipProvider>
+        <Tooltip>
+            <TooltipTrigger asChild>
+                {children}
+            </TooltipTrigger>
+            <TooltipContent align={"end"}>
+                <p>Expected: <Code>{expectedValue + ""}</Code></p>
+                <p>Actual: <Code>{actualValue + ""}</Code></p>
+            </TooltipContent>
+        </Tooltip>
+    </TooltipProvider>;
+
+}
+
+const Question = ({question, code, expectedValue, children}) => {
+    // variable for storing the result of the code evaluation
+    const [evaluationResult, setEvaluationResult] = useState(null);
+
+    const success = evaluationResult === null ? null : evaluationResult === expectedValue + "";
+
+    const evaluateCode = () => {
+        let result = null;
+        try {
+            result = eval(code);
+        } catch (e) {
+            result = e;
+        }
+
+        setEvaluationResult(result + "");
+    }
+
+    return (<>
+            <div className={"flex justify-between items-center"}>
+                <p>{question}</p>
+                {
+                    success === null
+                        ? <Button onClick={evaluateCode}>
+                            <Play className="mr-2 h-4 w-4"/> Run
+                        </Button>
+                        : success
+                            ? <Button className={"cursor-default disabled:opacity-100"} disabled
+                                      variant={"success"}><CheckSquare className="mr-2 h-4 w-4"/> Correct</Button>
+                            :
+                            <ResultButton expectedValue={expectedValue}
+                                          actualValue={evaluationResult}>
+                                <Button className={"cursor-help"}
+                                        variant={"destructive"}><X className="mr-2 h-4 w-4"/> Incorrect</Button>
+                            </ResultButton>
+                }
+            </div>
+            {children}
+        </>
+    );
+};
+
+export default Question;
